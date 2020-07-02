a8157a7f7f9b4d3c533d1dd2cbafdb968a96007e
Lucia Rumbo <lurumbo18@gmail.com>
2020-07-01 21:03:28 -0300

    initial commit

---
+++ src/main/java/com/pigmalion/springwebfluxreactivedemo/SpringWebfluxReactiveDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pigmalion.springwebfluxreactivedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxReactiveDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxReactiveDemoApplication.class, args);
+	}
+
+}
